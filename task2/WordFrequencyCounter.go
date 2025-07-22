65029b9e1aeb2658a96b4e7e7d4556ea104f0e48
uza2095 <129275743+uza2095@users.noreply.github.com>
2025-07-22 16:41:37 +0300

    edited WordFrequencyCounter.go

--- task2/WordFrequencyCounter.go
+++ task2/WordFrequencyCounter.go
@@ -1,28 +1,45 @@
 package main
-import ("fmt"
-		"strings"
-		"unicode"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
 )
-func counter(word string) map[rune]int{
-	count:=make(map[rune]int)
-	for _,r:=range word{
-		if unicode.IsLetter(r){
-			count[r]++
+
+func counter(input string) map[string]int {
+	count := make(map[string]int)
+	currentWord := strings.Builder{}
+
+	for _, r := range input {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			currentWord.WriteRune(unicode.ToLower(r))
+		} else {
+			if currentWord.Len() > 0 {
+				word := currentWord.String()
+				count[word]++
+				currentWord.Reset()
+			}
 		}
-		
+	}
+
+	if currentWord.Len() > 0 {
+		word := currentWord.String()
+		count[word]++
 	}
 	return count
 }
 
-func main(){
-	var word string
-	fmt.Println("please enter the word you want to count")
-	fmt.Scanln(&word)
-	edited:=strings.ToLower(word)
-	freq:=counter(edited)
-	for i,r:= range freq{
-		fmt.Printf("%c:%d\n",i,r)
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter the text you want to count:")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	freq := counter(input)
+
+	for word, count := range freq {
+		fmt.Printf("%s: %d\n", word, count)
 	}
-
-
 }
